531430f079c16a5e231e167ac29c1a56de176bb1
Arnaud <arnaudadjovi274@gmail.com>
2025-01-27 04:13:35 +0100

    Enhance Editor component: set height to 700 and enable syntax highlighting

--- src/pages/react-md-editor/index.tsx
+++ src/pages/react-md-editor/index.tsx
@@ -11,6 +11,8 @@ export default function Editor() {
         <MDEditor
           value={value}
           onChange={(value) => setValue(value || "")}
+          height={700}
+          highlightEnable={true}
         />
         <MDEditor.Markdown source={value} style={{ whiteSpace: 'pre-wrap' }} />
       </div>
